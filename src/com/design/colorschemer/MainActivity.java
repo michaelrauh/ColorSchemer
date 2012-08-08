4a78d470e601d958ec52fd82d8b2cb9689c67a56
Mark Tareshawty <tarebyte@gmail.com>
2012-08-08 13:29:34 -0400

    Made some ui changes

--- src/com/design/colorschemer/MainActivity.java
+++ src/com/design/colorschemer/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends Activity implements OnClickListener {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        
         // get the engines started
         init();
 
